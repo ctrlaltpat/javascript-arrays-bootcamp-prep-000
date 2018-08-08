3bfcf5a04db035af014b8b97e949bc662230bff7
patrickp246@gmail.com <patrickp246@gmail.com>
2018-08-08 09:35:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,5 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  
+  var newArray = array.slice(1);
+  return newArray;
 }
+
+function 
